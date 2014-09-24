6258ad590f92d47bc83d51fe19e99e3a9f611f01
Sergi Reyner <sergi@minerva.marplasport.local>
2014-09-24 11:14:01 +0100

    Character class is selected at the beginning of combat.

--- orc-battle.el
+++ orc-battle.el
@@ -105,7 +105,10 @@
 (defun init-player ()
   (setq orc-battle-player-health 30)
   (setq orc-battle-player-agility 30)
-  (setq orc-battle-player-strength 30))
+  (setq orc-battle-player-strength 30)
+  (setq orc-battle-player-class
+        (nth (1+ (random (length orc-battle-player-classes)))
+             orc-battle-player-classes)))
 
 (defun player-dead-p ()
   (<= orc-battle-player-health 0))
